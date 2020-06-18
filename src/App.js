8f733757394496d515c55585cb2d181bbef7e746
pupato13 <pupato.diego@gmail.com>
2020-06-18 19:58:46 +1200

    Fixing webpack package version problems to run tests. Fixing List refresh problem.

--- src/App.js
+++ src/App.js
@@ -9,130 +9,114 @@ import api from "./services/api";
 import InsertRepo from "./components/InsertRepo";
 
 function App() {
-    const [repositories, setRepositories] = useState([]);
+	const [repositories, setRepositories] = useState([]);
 
-    useEffect(() => {
-        api.get("repositories").then((response) => {
-            console.log(response);
-            setRepositories(response.data);
-        });
-    }, []);
+	useEffect(() => {
+		api.get("repositories").then((response) => {
+			console.log(response);
+			setRepositories(response.data);
+		});
+	}, []);
 
-    async function handleAddRepository() {
-        // TODO
-        let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
+	async function handleAddRepository() {
+		// TODO
+		let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
 
-        // if (!txtRepositoryTitle.value.trim()) {
-        //     txtRepositoryTitle.setAttribute("class", "textbox input-error");
-        //     return;
-        // }
+		// if (!txtRepositoryTitle.value.trim()) {
+		//     txtRepositoryTitle.setAttribute("class", "textbox input-error");
+		//     return;
+		// }
 
-        try {
-            await createRepository(txtRepositoryTitle.value);
-        } catch (err) {
-            alert("Ops! Something bad happened!");
-            console.log(err);
-        }
+		try {
+			await createRepository(txtRepositoryTitle.value);
+		} catch (err) {
+			alert("Ops! Something bad happened!");
+			console.log(err);
+		}
 
-        //alert(txtRepositoryName.value);
-    }
+		//alert(txtRepositoryName.value);
+	}
 
-    const createRepository = async (title) => {
-        var repository = {
-            title,
-            url: "teste",
-            techs: [],
-        };
+	const createRepository = async (title) => {
+		var repository = {
+			title,
+			url: "teste",
+			techs: [],
+		};
 
-        let response = await api.post("repositories", repository);
+		let response = await api.post("repositories", repository);
 
-        if (response.status === 200) {
-            console.log(response);
+		if (response.status === 200) {
+			console.log(response);
 
-            txtRepositoryTitle.setAttribute("class", "textbox");
-            txtRepositoryTitle.value = "";
+			txtRepositoryTitle.setAttribute("class", "textbox");
+			txtRepositoryTitle.value = "";
 
-            setRepositories([...repositories, response.data]);
-        }
-    };
+			setRepositories([...repositories, response.data]);
+		}
+	};
 
-    async function handleRemoveRepository(id) {
-        // TODO
-        console.log(id);
+	async function handleRemoveRepository(id) {
+		// TODO
+		let response = await api.delete(`repositories/${id}`);
 
-        //let repoIndex = repositories.findIndex((repo) => repo.id === id);
+		if (response.status === 204) {
+			refreshRepositories(id);
+		} else {
+			alert("Ops. Something bad happened!");
+		}
+	}
 
-        let response = await api.delete(`repositories/${id}`);
+	const refreshRepositories = (id) => {
+		const repos = [...repositories];
 
-        if (response.status === 204) {
-            //alert("Repository deleted!");
+		const repoIndex = repositories.findIndex((repo) => repo.id === id);
 
-            refreshRepositories(id);
-        }
+		repos.splice(repoIndex, 1);
 
-        //setRepositories([...repositories, {}]);
-        //let repos = repositories.filter((repo) => repo.id !== id);
+		setRepositories(repos);
+	};
 
-        //setRepositories([...repositories, repos]);
-    }
+	return (
+		<div>
+			<label className="label" htmlFor="txtRepositoryTitle">
+				Repository Title
+			</label>
+			<input
+				type="text"
+				className="textbox"
+				id="txtRepositoryTitle"
+				placeholder="Title"
+			/>
+			<label className="label" htmlFor="txtRepositoryUrl">
+				Repository URL
+			</label>
+			<input
+				type="text"
+				className="textbox"
+				id="txtRepositoryUrl"
+				placeholder="URL"
+			/>
+			<button className="btnAdicionar" onClick={handleAddRepository}>
+				Adicionar
+			</button>
 
-    const refreshRepositories = (id) => {
-        console.log(id);
-
-        //repositories.splice(repoIndex, 1);
-        let ulElement = document.querySelector("ul");
-        ulElement.innerHTML = "";
-
-        let repos = repositories.filter((repo) => repo.id !== id);
-
-        console.log(repos);
-
-        repos.map((repo) => {
-            let liElement = document.createElement("li");
-            liElement.innerHTML = repo.title;
-
-            let buttonElement = document.createElement("button");
-            buttonElement.innerHTML = "Remover";
-            buttonElement.onclick = () => handleRemoveRepository(repo.id);
-
-            liElement.appendChild(buttonElement);
-            ulElement.appendChild(liElement);
-        });
-
-        setRepositories([...repositories, repos]);
-    };
-
-    return (
-        <div>
-            <label className="label" htmlFor="txtRepositoryTitle">
-                Repository Title
-            </label>
-            <input
-                type="text"
-                className="textbox"
-                id="txtRepositoryTitle"
-                placeholder="Title"
-            />
-            <button className="btnAdicionar" onClick={handleAddRepository}>
-                Adicionar
-            </button>
-
-            <ul data-testid="repository-list">
-                {repositories.map((repository) => (
-                    <li key={repository.id}>
-                        {repository.title}
-                        <button
-                            onClick={() =>
-                                handleRemoveRepository(repository.id)
-                            }
-                        >
-                            Remover
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+			<ul data-testid="repository-list">
+				{repositories.map((repository) => (
+					<li key={repository.id}>
+						{repository.title}
+						<button
+							onClick={() =>
+								handleRemoveRepository(repository.id)
+							}
+						>
+							Remover
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
