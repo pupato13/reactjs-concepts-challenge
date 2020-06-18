5dafb2c36e023569831336fdd16e615f8a6f2a78
pupato13 <pupato.diego@gmail.com>
2020-06-18 21:47:25 +1200

    Challenge finished. I removed validation because it was crashing tests.

--- src/App.js
+++ src/App.js
@@ -2,62 +2,58 @@ import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
-//import "bootstrap/dist/css/bootstrap.min.css";
-
 import api from "./services/api";
-
-import InsertRepo from "./components/InsertRepo";
 
 function App() {
 	const [repositories, setRepositories] = useState([]);
 
 	useEffect(() => {
 		api.get("repositories").then((response) => {
-			console.log(response);
 			setRepositories(response.data);
 		});
 	}, []);
 
 	async function handleAddRepository() {
-		// TODO
-		let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
+		let title = document.getElementById("txtRepositoryTitle").value;
+		let url = document.getElementById("txtRepositoryUrl").value;
+		let techs = document
+			.getElementById("txtRepositoryTechs")
+			.value.split(",")
+			.map((item) => item.trim());
 
-		// if (!txtRepositoryTitle.value.trim()) {
-		//     txtRepositoryTitle.setAttribute("class", "textbox input-error");
-		//     return;
-		// }
+		//clearErrorClasses();
+
+		// Removendo validacao no front porque quebra os testes
+		//if (!isPostValid()) return;
 
 		try {
-			await createRepository(txtRepositoryTitle.value);
+			await createRepository(title, url, techs);
 		} catch (err) {
 			alert("Ops! Something bad happened!");
 			console.log(err);
 		}
-
-		//alert(txtRepositoryName.value);
 	}
 
-	const createRepository = async (title) => {
-		var repository = {
+	const createRepository = async (title, url, techs) => {
+		let repository = {
 			title,
-			url: "teste",
-			techs: [],
+			//url: "https://github.com/pupato13/reactjs-concepts-challenge",
+			url,
+			techs: [...techs],
 		};
 
 		let response = await api.post("repositories", repository);
 
 		if (response.status === 200) {
-			console.log(response);
-
-			txtRepositoryTitle.setAttribute("class", "textbox");
-			txtRepositoryTitle.value = "";
-
 			setRepositories([...repositories, response.data]);
+
+			clearInputs();
+		} else {
+			alert("Ops. Something bad happened!");
 		}
 	};
 
 	async function handleRemoveRepository(id) {
-		// TODO
 		let response = await api.delete(`repositories/${id}`);
 
 		if (response.status === 204) {
@@ -68,13 +64,60 @@ function App() {
 	}
 
 	const refreshRepositories = (id) => {
-		const repos = [...repositories];
+		let repos = [...repositories];
 
-		const repoIndex = repositories.findIndex((repo) => repo.id === id);
+		let repoIndex = repositories.findIndex((repo) => repo.id === id);
 
 		repos.splice(repoIndex, 1);
 
 		setRepositories(repos);
+	};
+
+	const isPostValid = () => {
+		let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
+		let txtRepositoryUrl = document.getElementById("txtRepositoryUrl");
+		let txtRepositoryTechs = document.getElementById("txtRepositoryTechs");
+
+		let isValid = true;
+
+		if (!txtRepositoryTitle.value.trim()) {
+			txtRepositoryTitle.setAttribute("class", "textbox input-error");
+			isValid = false;
+		}
+
+		if (!txtRepositoryUrl.value.trim()) {
+			txtRepositoryUrl.setAttribute("class", "textbox input-error");
+			isValid = false;
+		}
+
+		if (!txtRepositoryTechs.value.trim()) {
+			txtRepositoryTechs.setAttribute("class", "textbox input-error");
+			isValid = false;
+		}
+
+		return isValid;
+	};
+
+	const clearInputs = () => {
+		let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
+		let txtRepositoryUrl = document.getElementById("txtRepositoryUrl");
+		let txtRepositoryTechs = document.getElementById("txtRepositoryTechs");
+
+		txtRepositoryTitle.value = "";
+		txtRepositoryUrl.value = "";
+		txtRepositoryTechs.value = "";
+
+		clearErrorClasses();
+	};
+
+	const clearErrorClasses = () => {
+		let txtRepositoryTitle = document.getElementById("txtRepositoryTitle");
+		let txtRepositoryUrl = document.getElementById("txtRepositoryUrl");
+		let txtRepositoryTechs = document.getElementById("txtRepositoryTechs");
+
+		txtRepositoryTitle.setAttribute("class", "textbox");
+		txtRepositoryUrl.setAttribute("class", "textbox");
+		txtRepositoryTechs.setAttribute("class", "textbox");
 	};
 
 	return (
@@ -96,6 +139,15 @@ function App() {
 				className="textbox"
 				id="txtRepositoryUrl"
 				placeholder="URL"
+			/>
+			<label className="label" htmlFor="txtRepositoryTechs">
+				Techs <span className="label-sm">[Separe as techs por , ]</span>
+			</label>
+			<input
+				type="text"
+				className="textbox"
+				id="txtRepositoryTechs"
+				placeholder="ex: NodeJS, ReactJS, React Native"
 			/>
 			<button className="btnAdicionar" onClick={handleAddRepository}>
 				Adicionar
